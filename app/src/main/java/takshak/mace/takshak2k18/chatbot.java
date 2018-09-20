6a6a1a84b84458180385402eb21962239d2a137a
Aravind Balakrishnan <aravindbalan222@gmail.com>
2018-09-20 23:20:10 +0530

    ui change

--- app/src/main/java/takshak/mace/takshak2k18/chatbot.java
+++ app/src/main/java/takshak/mace/takshak2k18/chatbot.java
@@ -8,6 +8,7 @@ import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -48,6 +49,8 @@ public class chatbot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
         mListView = (ListView) findViewById(R.id.listView);
         mButtonSend = (FloatingActionButton) findViewById(R.id.btn_send);
         mEditTextMessage = (EditText) findViewById(R.id.et_message);
