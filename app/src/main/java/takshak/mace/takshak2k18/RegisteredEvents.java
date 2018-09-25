c165e2e530e05d38be15296186b9a427aa366bc5
Aravind Balakrishnan <aravindbalan222@gmail.com>
2018-09-25 20:28:37 +0530

    ui change

--- app/src/main/java/takshak/mace/takshak2k18/RegisteredEvents.java
+++ app/src/main/java/takshak/mace/takshak2k18/RegisteredEvents.java
@@ -14,13 +14,11 @@ public class RegisteredEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_events);
-
         eventList = new ArrayList<>();
         eventList.add("event1");
         eventList.add("event2");
         eventList.add("event3");
         eventList.add("event4");
-
         listView = findViewById(R.id.eventlistview);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_view, R.id.textviewMy, eventList);
         listView.setAdapter(arrayAdapter);
